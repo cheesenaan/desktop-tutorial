281460d6633644390b91c957f2caf92bac6dd877
Sulemaan Farooq <shf46@scarletmail.rutgers.edu>
2024-07-08 15:45:13 -0400

    Update H.js

--- H.js
+++ H.js
@@ -1,14 +1,8 @@
 api.get('/resume', async (req, res) => {
-    try {
-        const DAO = new resumeDataDAO();
-        const data = DAO.getAllUserData();
-        console.log("from object:", data);
-        res.status(200).json(data);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+  try {
+    const data = await fetchDataAndLog();
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
-
-exports.handler = async (event, context) => {
-    return await api.run(event, context);
-};
