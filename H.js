d4a744bce35478e65f818c39edf938fb433a42e0
Sulemaan Farooq <shf46@scarletmail.rutgers.edu>
2024-07-09 16:13:15 -0400

    Update H.js

--- H.js
+++ H.js
@@ -1,59 +1,76 @@
+import React from 'react';
+import Carousel from 'some-carousel-library'; // Replace with the actual import
 
-<Carousel
-    layout="3UP"
-    gutter="24px"
-    peek="standard"
-    surface="light"
-    paginationInset="12px"
-    pagination={{
-      kind: 'lowContrast',
-      hideBorder: true,
-      floating: true,
-    }}
-    paginationDisplay="persistent"
-    aspectRatio="16:9"
-    data={[
-    
-      {
-        title: {
-          size: 'titleLarge',
-          primitive: 'h1',
-          children: `Up to 30% off gifts`,
+const MyCarousel = () => {
+  return (
+    <Carousel
+      layout="3UP"
+      gutter="24px"
+      peek="standard"
+      surface="light"
+      paginationInset="12px"
+      pagination={{
+        kind: 'lowContrast',
+        hideBorder: true,
+        floating: true,
+      }}
+      paginationDisplay="persistent"
+      aspectRatio="16:9"
+      data={[
+        {
+          title: {
+            size: 'titleLarge',
+            primitive: 'h1',
+            children: `Up to 30% off gifts`,
+          },
+          backgroundColor: 'white',
+          showBorder: true,
+          onClick: () => {},
         },
-        backgroundColor: 'white',
-        
-        showBorder: true,
-        onClick: () => {},
-      },
-      {
-        title: {
-          size: 'titleLarge',
-          primitive: 'h1',
-          children: 'Get 5G Home Internet starting at $25/mo.',
+        {
+          title: {
+            size: 'titleLarge',
+            primitive: 'h1',
+            children: 'Get 5G Home Internet starting at $25/mo.',
+          },
+          backgroundColor: 'black',
+          onClick: () => {},
         },
-        backgroundColor: 'black',
-        
-        onClick: () => {},
-      },
-      {
-        title: {
-          size: 'titleLarge',
-          primitive: 'h1',
-          children: 'Get Unlimited starting at $35/mo.',
+        {
+          title: {
+            size: 'titleLarge',
+            primitive: 'h1',
+            children: 'Get Unlimited starting at $35/mo.',
+          },
+          backgroundColor: 'white',
+          showBorder: true,
+          onClick: () => {},
         },
-        backgroundColor: 'white',
-        showBorder: true,
-        onClick: () => {},
-      },
-      {
-        title: {
-          size: 'titleLarge',
-          primitive: 'h1',
-          children: 'Get 35% off accessories when you buy 4 or more',
+        {
+          title: {
+            size: 'titleLarge',
+            primitive: 'h1',
+            children: 'Get 35% off accessories when you buy 4 or more',
+          },
+          backgroundColor: 'gray',
+          onClick: () => {},
         },
-        backgroundColor: 'gray',
-        onClick: () => {},
-      },
-    ]}
-    renderItem={(props) => <data {...props} width="100%" />}
-  />
+      ]}
+      renderItem={({ item }) => (
+        <div
+          style={{
+            backgroundColor: item.backgroundColor,
+            border: item.showBorder ? '1px solid black' : 'none',
+            padding: '16px',
+            textAlign: 'center',
+          }}
+          onClick={item.onClick}
+        >
+          <h1 style={{ fontSize: item.title.size }}>{item.title.children}</h1>
+        </div>
+      )}
+    />
+  );
+};
+
+export default MyCarousel;
