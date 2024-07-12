931eacaa424212921e50254e5d20db5bc5a8c1e0
Sulemaan Farooq <shf46@scarletmail.rutgers.edu>
2024-07-12 14:53:00 -0400

    Update H.js

--- H.js
+++ H.js
@@ -1,76 +1,56 @@
-import React from 'react';
-import Carousel from 'some-carousel-library'; // Replace with the actual import
+import React, { Component } from 'react';
 
-const MyCarousel = () => {
-  return (
-    <Carousel
-      layout="3UP"
-      gutter="24px"
-      peek="standard"
-      surface="light"
-      paginationInset="12px"
-      pagination={{
-        kind: 'lowContrast',
-        hideBorder: true,
-        floating: true,
-      }}
-      paginationDisplay="persistent"
-      aspectRatio="16:9"
-      data={[
-        {
-          title: {
-            size: 'titleLarge',
-            primitive: 'h1',
-            children: `Up to 30% off gifts`,
-          },
-          backgroundColor: 'white',
-          showBorder: true,
-          onClick: () => {},
-        },
-        {
-          title: {
-            size: 'titleLarge',
-            primitive: 'h1',
-            children: 'Get 5G Home Internet starting at $25/mo.',
-          },
-          backgroundColor: 'black',
-          onClick: () => {},
-        },
-        {
-          title: {
-            size: 'titleLarge',
-            primitive: 'h1',
-            children: 'Get Unlimited starting at $35/mo.',
-          },
-          backgroundColor: 'white',
-          showBorder: true,
-          onClick: () => {},
-        },
-        {
-          title: {
-            size: 'titleLarge',
-            primitive: 'h1',
-            children: 'Get 35% off accessories when you buy 4 or more',
-          },
-          backgroundColor: 'gray',
-          onClick: () => {},
-        },
-      ]}
-      renderItem={({ item }) => (
-        <div
-          style={{
-            backgroundColor: item.backgroundColor,
-            border: item.showBorder ? '1px solid black' : 'none',
-            padding: '16px',
-            textAlign: 'center',
-          }}
-          onClick={item.onClick}
-        >
-          <h1 style={{ fontSize: item.title.size }}>{item.title.children}</h1>
-        </div>
-      )}
-    />
-  );
-};
+export default class App extends Component {
+  render() {
+    const handleBuildResumeClick = async () => {
+      try {
+        const response = await fetch('http://localhost:3000/resume?user_id=1', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        });
 
-export default MyCarousel;
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+
+        const data = await response.json();
+        alert('Resume built successfully!');
+        console.log(data);
+      } catch (error) {
+        console.error('Error fetching resume:', error);
+        alert('Error building resume');
+      }
+    };
+
+    return (
+      <div style={resumeSubmitButton}>
+        <br /><br /><br /><br /><br />
+        <ButtonGroup
+          childWidth={'100%'}
+          viewport={'desktop'}
+          rowQuantity={{ desktop: 2 }}
+          data={[
+            {
+              children: 'Build resume',
+              size: 'large',
+              use: 'primary',
+              width: 'auto',
+              onClick: handleBuildResumeClick
+            },
+            {
+              children: 'Cancel',
+              size: 'large',
+              use: 'textLink',
+              width: 'auto',
+              onClick: () => alert('You clicked the Cancel button!')
+            }
+          ]}
+          alignment={'center'}
+        />
+        <br /><br /><br /><br /><br />
+      </div>
+    );
+  }
+}
