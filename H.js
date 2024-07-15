70c00f6b99d1551c369b58f53280b8cc49547afc
Sulemaan Farooq <shf46@scarletmail.rutgers.edu>
2024-07-15 14:46:10 -0400

    Update H.js

--- H.js
+++ H.js
@@ -8,3 +8,5 @@
     "languages" : "english, spanish"
 }
 
+INSERT INTO user_data (first_name, last_name, phone, email, location, languages) 
+VALUES ('sulemaan-test', 'farooq-test', '9174565697', 'test@gmail.com', 'Alabama', 'english, spanish');
