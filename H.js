49f5cbe26b94cdd325005caf29566e0cd12391b9
Sulemaan Farooq <shf46@scarletmail.rutgers.edu>
2024-07-16 11:20:06 -0400

    Update H.js

--- H.js
+++ H.js
@@ -1,18 +1,60 @@
-App.js:85 
- POST http://127.0.0.1:3001/api/v1/saveUserData 403 (FORBIDDEN)
-App.handleSubmit	@	App.js:85
-callCallback	@	react-dom.development.js:4164
-invokeGuardedCallbackDev	@	react-dom.development.js:4213
-invokeGuardedCallback	@	react-dom.development.js:4277
-invokeGuardedCallbackAndCatchFirstError	@	react-dom.development.js:4291
-executeDispatch	@	react-dom.development.js:9041
-processDispatchQueueItemsInOrder	@	react-dom.development.js:9073
-processDispatchQueue	@	react-dom.development.js:9086
-dispatchEventsForPlugins	@	react-dom.development.js:9097
-(anonymous)	@	react-dom.development.js:9288
-batchedUpdates$1	@	react-dom.development.js:26179
-batchedUpdates	@	react-dom.development.js:3991
-dispatchEventForPluginEventSystem	@	react-dom.development.js:9287
-dispatchEventWithEnableCapturePhaseSelectiveHydrationWithoutDiscreteEventReplay	@	react-dom.development.js:6465
-dispatchEvent	@	react-dom.development.js:6457
-dispatchDiscreteEvent
+const api = require('lambda-api')();
+require('dotenv').config();
+const ResumeDataDAO = require('./ResumeDataDAO');
+
+api.get('/api/v2/hello', async (req, res) => {
+    try {
+        const data = 'hello world!';
+        res.status(200).json(data);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+api.get('/api/v2/home', async (req, res) => {
+    try {
+        const data = 'welcome to the home page!';
+        res.status(200).json(data);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+api.get('/api/v2/resume', async (req, res) => {
+    try {
+        const { user_id } = req.query; // Get user_id from query parameters
+        if (!user_id) {
+            return res.status(400).json({ error: 'user_id is required' });
+        }
+
+        const resumeDataDAO = new ResumeDataDAO();
+        const data = await resumeDataDAO.getResumeData(user_id);
+        res.status(200).json(data);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+
+api.post('/api/v1/saveUserData', async (req, res) => {
+    console.log("inside index ");
+    const {first_name, last_name, phone, email, location, languages } = req.body;
+
+    // Validate input fields if needed
+
+    const resumeDataDAO = new ResumeDataDAO();
+
+    try {
+        await resumeDataDAO.sendUserData(first_name, last_name, phone, email, location, languages);
+        res.status(200).send("Data saved successfully");
+    } catch (error) {
+        console.error('Error saving user data:', error.message);
+        res.status(500).send('Failed to save data');
+    }
+});
+  
+
+
+exports.handler = async (event, context) => {
+    return await api.run(event, context);
+};
