f825e8ed760865a4364929def17bf814b1e7fefd
Sulemaan Farooq <shf46@scarletmail.rutgers.edu>
2024-07-16 10:34:52 -0400

    Update H.js

--- H.js
+++ H.js
@@ -1,2 +1,5 @@
-INSERT INTO user_data (first_name, last_name, phone, email, location, languages)
-VALUES ('John', 'Doe', 1234567890, 'john.doe@example.com', 'New York', 'English, Spanish');
+ERROR:  Failing row contains (null, John, Doe, 1234567890, john.doe@example.com, New York, English, Spanish).null value in column "user_id" of relation "user_data" violates not-null constraint 
+
+ERROR:  null value in column "user_id" of relation "user_data" violates not-null constraint
+SQL state: 23502
+Detail: Failing row contains (null, John, Doe, 1234567890, john.doe@example.com, New York, English, Spanish).
