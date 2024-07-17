d3f808a154a7a7f83a9c5d4c33c676ccc667af15
Sulemaan Farooq <shf46@scarletmail.rutgers.edu>
2024-07-17 16:33:41 -0400

    Update H.js

--- H.js
+++ H.js
@@ -1,58 +1,58 @@
 {resumeData && (
-            <div>
-              <h3>Resume Data:</h3>
-              <pre>{JSON.stringify(resumeData, null, 2)}</pre>
+  <div>
+    <h3>Resume Data:</h3>
+    <pre>{JSON.stringify(resumeData, null, 2)}</pre>
 
-                <div id='display_resume_1' style={display_resume_1_style}> 
-                    <div id='display_resume_1_text_area' style={display_resume_1_text_area_style}> 
+    <div id='display_resume_1' style={display_resume_1_style}> 
+      <div id='display_resume_1_text_area' style={display_resume_1_text_area_style}> 
+        <br /><br /><br /><br /><br /><br />
 
-                    <br /><br /><br /><br ></br> <br /><br />
+        <TitleLockup
+          id='hero-title'
+          surface="dark"
+          data={{
+            title: {
+              size: 'title2XLarge',
+              children: `${resumeData.first_name} ${resumeData.last_name}`,
+            },
+            subtitle: {
+              size: 'titleMedium',
+              children: `${resumeData.phone} ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎  ${resumeData.email} ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ${resumeData.location} ‎ ‎ ‎  ‎ ‎ ‎ ‎ ‎ ‎ ${resumeData.languages}`,
+            },
+          }}
+        />   
+        <br /> 
 
+        <TitleLockup
+          id='hero-title'
+          surface="dark"
+          data={{
+            eyebrow: {
+              size: 'title2XLarge',
+              children: 'Please click continue to view your resume powered by the fastest 5G in the world',
+            },
+          }}
+        />
+        <br /> <br />
 
-                      <TitleLockup id='hero-title'
-                        surface="dark"
-                        data={{
-                          title: {
-                            size: 'title2XLarge',
-                            children: 'resumeData.first_name resumeData.last_name.',
-                          },
-                          subtitle: {
-                            size: 'titleMedium',
-                            children: '{{resumeData.phone}} ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎  {{resumeData.email}} ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ {{resumeData.location}} ‎ ‎ ‎  ‎ ‎ ‎ ‎ ‎ ‎ {{languages}}.',
-                            
-                          },
-                        }}
-                      />   <br /> 
-
-                      <TitleLockup id='hero-title'
-                        surface="dark"
-                        data={{
-                          eyebrow: {
-                            size: 'title2XLarge',
-                            children: 'Please click continue to view your resume powered by the fastest 5G in the world',
-                          },
-                        }}
-                      />
-                      <br /> <br />
-
-                      <ButtonGroup id='hero-buttons'
-                          childWidth={'100%'}
-                          viewport={'desktop'}
-                          surface="dark"
-                          rowQuantity={{ desktop: 2 }}
-                          alignment={'left'}
-                          data={[
-                            {
-                              children: 'Continue',
-                              size: 'medium',
-                              use: 'secondary',
-                              width: '150px',
-                              onClick: () => alert('You clicked the Button example!'),
-                            },]}
-                        />;
-                    </div>
-                </div> 
-
-            </div>
-
-          )}
+        <ButtonGroup
+          id='hero-buttons'
+          childWidth={'100%'}
+          viewport={'desktop'}
+          surface="dark"
+          rowQuantity={{ desktop: 2 }}
+          alignment={'left'}
+          data={[
+            {
+              children: 'Continue',
+              size: 'medium',
+              use: 'secondary',
+              width: '150px',
+              onClick: () => alert('You clicked the Button example!'),
+            },
+          ]}
+        />
+      </div>
+    </div>
+  </div>
+)}
