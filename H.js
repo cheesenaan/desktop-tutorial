75260ef689f8b37e27fe5ae20a33a8ad4b0e5533
Sulemaan Farooq <shf46@scarletmail.rutgers.edu>
2024-07-18 15:41:54 -0400

    Update H.js

--- H.js
+++ H.js
@@ -1,40 +1,43 @@
-return (
-      <div style={{ padding: '20px' }}>
-        <div style={{ marginBottom: '20px' }}>
-          <ButtonGroup
-            childwidth="100%"
-            viewport="desktop"
-            rowQuantity={{ desktop: 2 }}
-            data={[
-              {
-                children: 'Get resume data (test)',
-                size: 'large',
-                use: 'primary',
-                width: 'auto',
-                onClick: this.getResumeData,
-              },
-              {
-                children: 'Cancel',
-                size: 'large',
-                use: 'textLink',
-                width: 'auto',
-              },
-            ]}
-            alignment="center"
-          />
-        </div>
-
-        {error && <div style={{ color: 'red' }}>{error}</div>}
-
-        {resumeData && (
-          <div>
-            <h3>Resume Data:</h3>
-            <pre>{JSON.stringify(resumeData, null, 2)}</pre>
-          </div>
-        )}
-      </div>
-    );
-  }
+{
+  "user_data": [
+    {
+      "user_id": 34,
+      "first_name": "sulemaan",
+      "last_name": "farooq",
+      "phone": "1111111111",
+      "email": "test@verizon.com",
+      "location": "NJ",
+      "languages": "English, Spansish"
+    }
+  ],
+  "education": [
+    {
+      "education_id": 29,
+      "university": "Rutgers",
+      "major": "Computer Science",
+      "gpa": 3.5,
+      "coursework": "AI, ML",
+      "location": "NJ",
+      "user_id": 34
+    }
+  ],
+  "work_experience": [
+    {
+      "work_experience_id": 22,
+      "title": "Software Engineer",
+      "company": "Verizon",
+      "description": "full stack engineer. node js, aws sam, yaml, react, jenkins",
+      "start_date": "2023-01-01T00:00:00.000Z",
+      "end_date": "2024-01-01T00:00:00.000Z",
+      "user_id": 34
+    }
+  ],
+  "projects": [
+    {
+      "project_id": 22,
+      "title": "trading app",
+      "description": "flask app to trade 1500 stocks",
+      "user_id": 34
+    }
+  ]
 }
-
-export default DisplayResume;
