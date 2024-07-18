e735f7ad428487c0fff06ad50baf6aa965f1ffc4
Sulemaan Farooq <shf46@scarletmail.rutgers.edu>
2024-07-18 15:43:56 -0400

    Update H.js

--- H.js
+++ H.js
@@ -1,43 +1,47 @@
-{
-  "user_data": [
-    {
-      "user_id": 34,
-      "first_name": "sulemaan",
-      "last_name": "farooq",
-      "phone": "1111111111",
-      "email": "test@verizon.com",
-      "location": "NJ",
-      "languages": "English, Spansish"
-    }
-  ],
-  "education": [
-    {
-      "education_id": 29,
-      "university": "Rutgers",
-      "major": "Computer Science",
-      "gpa": 3.5,
-      "coursework": "AI, ML",
-      "location": "NJ",
-      "user_id": 34
-    }
-  ],
-  "work_experience": [
-    {
-      "work_experience_id": 22,
-      "title": "Software Engineer",
-      "company": "Verizon",
-      "description": "full stack engineer. node js, aws sam, yaml, react, jenkins",
-      "start_date": "2023-01-01T00:00:00.000Z",
-      "end_date": "2024-01-01T00:00:00.000Z",
-      "user_id": 34
-    }
-  ],
-  "projects": [
-    {
-      "project_id": 22,
-      "title": "trading app",
-      "description": "flask app to trade 1500 stocks",
-      "user_id": 34
-    }
-  ]
-}
+<div id='display_resume_1' style={display_resume_1_style}>
+            <div id='display_resume_1_text_area'>
+              <TitleLockup
+                id='hero-title'
+                surface="dark"
+                data={{
+                  title: {
+                    size: 'title2XLarge',
+                    children: "`${resumeData.user_data[0].first_name} ${resumeData.user_data[0].last_name}`",
+                  },
+                  subtitle: {
+                    size: 'titleMedium',
+                    children: "`${resumeData.user_data[0].phone} ${resumeData.user_data[0].email} ${resumeData.user_data[0].location} ${resumeData.user_data[0].languages}`",
+                  },
+                }}
+              />
+
+              <TitleLockup
+                id='hero-title'
+                surface="dark"
+                data={{
+                  eyebrow: {
+                    size: 'title2XLarge',
+                    children: 'Please click continue to view your resume powered by the fastest 5G in the world',
+                  },
+                }}
+              />
+
+              <ButtonGroup
+                id='hero-buttons'
+                childWidth={'100%'}
+                viewport={'desktop'}
+                surface="dark"
+                rowQuantity={{ desktop: 2 }}
+                alignment={'left'}
+                data={[
+                  {
+                    children: 'Continue',
+                    size: 'medium',
+                    use: 'secondary',
+                    width: '150px',
+                    onClick: () => alert('You clicked the Button example!'),
+                  },
+                ]}
+              />
+            </div>
+          </div>
