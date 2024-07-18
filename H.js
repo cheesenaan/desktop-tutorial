7f3c83b21649f33e489832b3a7ea722b072d92c5
Sulemaan Farooq <shf46@scarletmail.rutgers.edu>
2024-07-18 13:48:43 -0400

    Update H.js

--- H.js
+++ H.js
@@ -1,6 +1,1 @@
-if (response.ok) {
-        const data = await response.json();
-        const { success, message, user_ID } = data;
-        if (success) {
-          alert(`${message} User ID: ${user_ID}`);
-          this.setState({userId : ${user_ID});
+this.setState({ userId: user_ID });
