d9ca7b0a17488903b9ad22e8183555dd8be0252e
Sulemaan Farooq <shf46@scarletmail.rutgers.edu>
2024-07-18 12:11:43 -0400

    Update H.js

--- H.js
+++ H.js
@@ -1,19 +1,27 @@
-  getResumeData = async () => {
-    try {
-      const { userId } = this.state; 
-      if (userId == null) {
-        
-      }
-      const response = await fetch(`api/v2/resume?user_id=${userId}`, {
-        mode: 'no-cors',
-      });
-      if (!response.ok) {
-        throw new Error(`Error: ${response.statusText}`);
-      }
+ saveUserData = async (event) => {
+    event.preventDefault();
+    const { first_name, last_name, phone, email, location, languages, university, university_location, major, gpa, coursework, company1,jobTitle1, startDate1, endDate1, description1, projectTitle1, projectDescription1 } = this.state;
+    console.log(first_name, last_name, phone, email, location, languages, university, university_location, major, gpa, coursework, company1,jobTitle1, startDate1, endDate1, description1, projectTitle1, projectDescription1);
+
+    const response = await fetch('api/v2/saveUserData', {
+      mode : 'no-cors',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ first_name, last_name, phone, email, location, languages, university, university_location, major, gpa, coursework, company1,jobTitle1, startDate1, endDate1, description1, projectTitle1, projectDescription1 }),
+    });
+
+    if (response.ok) {
       const data = await response.json();
-      this.setState({ resumeData: data, error: null });
-    } catch (error) {
-      console.error('Failed to fetch resume data:', error);
-      this.setState({ error: 'Failed to fetch resume data. Please try again later.' });
+      const { success, message, userId } = data;
+      if (success) {
+        this.setState({ userId });
+        alert(`${message} User ID: ${userId}`);
+      } else {
+        alert('User Data save failed!');
+      }
+    } else {
+      alert('Network error or failed to fetch');
     }
   };
