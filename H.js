61144b32beb295d728b955b98d2fb482a13b6e68
Sulemaan Farooq <shf46@scarletmail.rutgers.edu>
2024-07-18 13:38:06 -0400

    Update H.js

--- H.js
+++ H.js
@@ -1,5 +1,30 @@
 app.js
 
+constructor(props) {
+    super(props);
+    this.state = {
+      userId: null,
+      first_name: '',
+      last_name: '',
+      phone: '',
+      email: '',
+      location: '',
+      languages: '',
+      university: '',
+      university_location: '',
+      major: '',
+      gpa: '',
+      coursework: '',
+      company1: '',
+      jobTitle1: '',
+      startDate1: '',
+      endDate1: '',
+      description1: '',
+      projectTitle1: '',
+      projectDescription1: '',
+    };
+  }
+
 saveUserData = async (event) => {
     event.preventDefault();
   
