ba03e53737d7657ca8ca97a31b2457e18abcd7d7
Sulemaan Farooq <shf46@scarletmail.rutgers.edu>
2024-07-29 16:39:51 -0400

    Update H.js

--- H.js
+++ H.js
@@ -1,67 +1,178 @@
-resources:
-  # application load balancer
-  myalb:
-    type: aws::elasticloadbalancingv2::loadbalancer
-    properties:
-      subnets:
-        - !ref publicsubnet1
-        - !ref publicsubnet2
-      securitygroups:
-        - !ref albsecuritygroup
-      type: application
+AWSTemplateFormatVersion: '2010-09-09'
+Description: CloudFormation Template for ALB with Lambda functions and routing rules
 
-  # rule will be for anything coming in on port 80 (http) to redirect to port 443 (https)
-  alblistener:
-    type: aws::elasticloadbalancingv2::listener
-    properties:
-      loadbalancerarn: !ref myalb
-      port: 80
-      protocol: http
-      defaultactions:
-        - type: redirect
-          redirectconfig:
-            protocol: https
-            port: 443
-            statuscode: http_301
+Resources:
+  # Application Load Balancer
+  MyALB:
+    Type: AWS::ElasticLoadBalancingV2::LoadBalancer
+    Properties:
+      Name: my-alb
+      Subnets:
+        - !Ref PublicSubnet1  # Replace with your public subnet ID
+        - !Ref PublicSubnet2  # Replace with your public subnet ID
+      SecurityGroups:
+        - !Ref ALBSecurityGroup  # Replace with your ALB security group ID
+      Scheme: internet-facing
+      Type: application
+      Tags:
+        - Key: Environment
+          Value: J6YV
 
-  
-# rule for anything coming to /api/v1 to point to the resume lambda
-ResumeLambda": {
-        "Type": "AWS::Serverless::Function",
-        "Properties": {
-          "Handler": "index.handler",
-          "Runtime": "nodejs14.x",
-          "CodeUri": "s3://BUCKET-S3/resume-function.zip",
-          "Tags": {
-            "Project": "j6yv"
-          }
-        }
+  # HTTP Listener to redirect HTTP traffic to HTTPS
+  ALBListenerHTTP:
+    Type: AWS::ElasticLoadBalancingV2::Listener
+    Properties:
+      DefaultActions:
+        - Type: redirect
+          RedirectConfig:
+            Protocol: HTTPS
+            Port: 443
+            StatusCode: HTTP_301  # Redirect HTTP to HTTPS
+      LoadBalancerArn: !Ref MyALB
+      Port: 80
+      Protocol: HTTP
+      Tags:
+        - Key: Environment
+          Value: J6YV
 
+  # HTTPS Listener for the ALB
+  ALBListenerHTTPS:
+    Type: AWS::ElasticLoadBalancingV2::Listener
+    Properties:
+      DefaultActions:
+        - Type: fixed-response
+          FixedResponseConfig:
+            StatusCode: 200
+            ContentType: text/plain
+            MessageBody: Default response
+      LoadBalancerArn: !Ref MyALB
+      Port: 443
+      Protocol: HTTPS
+      SslPolicy: ELBSecurityPolicy-2016-08
+      Certificates:
+        - CertificateArn: !Ref YourSSLCertificateArn  # Replace with your SSL certificate ARN
+      Tags:
+        - Key: Environment
+          Value: J6YV
 
-  # default rule where all other traffic is directed to the reader lambda
+  # Target Group for Resume Lambda
+  ResumeTargetGroup:
+    Type: AWS::ElasticLoadBalancingV2::TargetGroup
+    Properties:
+      Name: ResumeTargetGroup
+      Port: 80
+      Protocol: HTTP
+      VpcId: !Ref VPCId  # Replace with your VPC ID
+      HealthCheckPath: /
+      HealthCheckProtocol: HTTP
+      HealthCheckIntervalSeconds: 30
+      HealthCheckTimeoutSeconds: 5
+      HealthyThresholdCount: 2
+      UnhealthyThresholdCount: 2
+      Tags:
+        - Key: Environment
+          Value: J6YV
 
+  # Target Group for Reader Lambda
+  ReaderTargetGroup:
+    Type: AWS::ElasticLoadBalancingV2::TargetGroup
+    Properties:
+      Name: ReaderTargetGroup
+      Port: 80
+      Protocol: HTTP
+      VpcId: !Ref VPCId  # Replace with your VPC ID
+      HealthCheckPath: /
+      HealthCheckProtocol: HTTP
+      HealthCheckIntervalSeconds: 30
+      HealthCheckTimeoutSeconds: 5
+      HealthyThresholdCount: 2
+      UnhealthyThresholdCount: 2
+      Tags:
+        - Key: Environment
+          Value: J6YV
 
+  # Listener Rule for /api/v1 to point to Resume Lambda
+  ALBListenerRuleAPI:
+    Type: AWS::ElasticLoadBalancingV2::ListenerRule
+    Properties:
+      Actions:
+        - Type: forward
+          TargetGroupArn: !Ref ResumeTargetGroup
+      Conditions:
+        - Field: path-pattern
+          PathPatternConfig:
+            Values:
+              - /api/v1/*
+      ListenerArn: !Ref ALBListenerHTTPS
+      Priority: 1
+      Tags:
+        - Key: Environment
+          Value: J6YV
 
-# this is the resume lambda
+  # Default Listener Rule to point to Reader Lambda
+  ALBListenerRuleDefault:
+    Type: AWS::ElasticLoadBalancingV2::ListenerRule
+    Properties:
+      Actions:
+        - Type: forward
+          TargetGroupArn: !Ref ReaderTargetGroup
+      Conditions: []
+      ListenerArn: !Ref ALBListenerHTTPS
+      Priority: 2
+      Tags:
+        - Key: Environment
+          Value: J6YV
 
-  # this is the reader lambda
-  "ReaderLambda": {
-  "Type": "AWS::Serverless::Function",
-  "Properties": {
-    "Handler": "index.handler",
-    "Runtime": "nodejs14.x",
-    "CodeUri": "s3://BUCKET-S3/reader-function.zip",
-    "Tags": {
-      "Project": "j6yv"
-    }
-  }
-}
+  # Lambda function for resume
+  ResumeLambda:
+    Type: AWS::Serverless::Function
+    Properties:
+      Handler: index.handler
+      Runtime: nodejs14.x
+      CodeUri: s3://BUCKET-S3/resume-function.zip  # Replace with your S3 bucket and key
+      Tags:
+        - Key: Project
+          Value: J6YV
+      Role: !Ref LambdaExecutionRole  # Replace with your Lambda execution role ARN
 
+  # Lambda function for reader
+  ReaderLambda:
+    Type: AWS::Serverless::Function
+    Properties:
+      Handler: index.handler
+      Runtime: nodejs14.x
+      CodeUri: s3://BUCKET-S3/reader-function.zip  # Replace with your S3 bucket and key
+      Tags:
+        - Key: Project
+          Value: J6YV
+      Role: !Ref LambdaExecutionRole  # Replace with your Lambda execution role ARN
 
+Outputs:
+  ALBDNSName:
+    Description: "DNS name of the Application Load Balancer"
+    Value: !GetAtt MyALB.DNSName
 
+Parameters:
+  VPCId:
+    Description: "The ID of the VPC"
+    Type: AWS::EC2::VPC::Id
 
-# Replace the following placeholders:
-# - PublicSubnet1, PublicSubnet2: IDs of your public subnets.
-# - ALBSecurityGroup: Security group for the ALB.
-# - YourSSLCertificateArn: ARN of your SSL certificate in ACM.
-# - YourTargetGroup:  ARN of the target group for your application.
+  PublicSubnet1:
+    Description: "The ID of the first public subnet"
+    Type: AWS::EC2::Subnet::Id
+
+  PublicSubnet2:
+    Description: "The ID of the second public subnet"
+    Type: AWS::EC2::Subnet::Id
+
+  ALBSecurityGroup:
+    Description: "The security group for the ALB"
+    Type: AWS::EC2::SecurityGroup::Id
+
+  YourSSLCertificateArn:
+    Description: "The ARN of the SSL certificate"
+    Type: String
+
+  LambdaExecutionRole:
+    Description: "IAM role for Lambda execution"
+    Type: AWS::IAM::Role::Arn
