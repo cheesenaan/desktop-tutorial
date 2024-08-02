09733c1482e90ad708fe30bc6466a1fcf9fb27af
Sulemaan Farooq <shf46@scarletmail.rutgers.edu>
2024-08-02 08:56:46 -0400

    Update H.js

--- H.js
+++ H.js
@@ -17,13 +17,9 @@ Resources:
         SecurityGroupIds: !Ref SecurityGroupsIds
         SubnetIds: !Ref SubnetIds
       Tags:
-        - Key: "ResumeFunction"
-          Value: resumefunction
-        - Key: UserID
-          Value: !Ref UserID
-        - Key: Vsad
-          Value: !Ref AppID
-
+        ResumeFunction: resumefunction
+        UserID: !Ref UserID
+        Vsad: !Ref AppID
 
   ReaderFunction:
     Type: 'AWS::Serverless::Function'
@@ -43,57 +39,6 @@ Resources:
         SecurityGroupIds: !Ref SecurityGroupsIds
         SubnetIds: !Ref SubnetIds
       Tags:
-        - Key: "ReaderFunction"
-          Value: resumefunction
-        - Key: UserID
-          Value: !Ref UserID
-        - Key: Vsad
-          Value: !Ref AppID
-
-  LoadBalance
-
-TASK [LaunchCF : Launch Cloudformation Stack] **********************************
-
-
-
-fatal: [localhost]: FAILED! => {
-
-    "changed": true,
-
-    "events": [
-
-        "StackEvent AWS::CloudFormation::Stack GTS-J6YV-VLDP-UPSKILL-SULEMAAN-APP-ALB CREATE_FAILED",
-
-        "StackEvent AWS::CloudFormation::Stack GTS-J6YV-VLDP-UPSKILL-SULEMAAN-APP-ALB CREATE_IN_PROGRESS",
-
-        "StackEvent AWS::CloudFormation::Stack GTS-J6YV-VLDP-UPSKILL-SULEMAAN-APP-ALB CREATE_IN_PROGRESS"
-
-    ],
-
-    "log": [
-
-        "AWS::CloudFormation::Stack GTS-J6YV-VLDP-UPSKILL-SULEMAAN-APP-ALB CREATE_FAILED: Transform AWS::Serverless-2016-10-31 failed with: Invalid Serverless Application Specification document. Number of errors found: 2. Resource with id [ResumeFunction] is invalid. Property 'Tags' should be a map. Resource with id [ReaderFunction] is invalid. Property 'Tags' should be a map."
-
-    ],
-
-    "output": "Stack CREATE failed",
-
-    "stack_outputs": {},
-
-    "stack_resources": []
-
-}
-
-PLAY RECAP *********************************************************************
-
-localhost                  : ok=26   changed=10   unreachable=0    failed=1    skipped=22   rescued=0    ignored=0   
-
-localhost                  : ok=26   changed=10   unreachable=0    failed=1    skipped=22   rescued=0    ignored=0   
-
-
-
-#$$PLAYBOOK EXECUTION COMPLETED$$#
-
-
-
-[Pipeline] }
+        ReaderFunction: resumefunction
+        UserID: !Ref UserID
+        Vsad: !Ref AppID
