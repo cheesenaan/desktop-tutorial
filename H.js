a2056a0b5efd4f795e249a4d8d78a97774f34386
Sulemaan Farooq <shf46@scarletmail.rutgers.edu>
2024-08-14 13:21:50 -0400

    Update H.js

--- H.js
+++ H.js
@@ -1,63 +1,360 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import Loader from 'path-to-loader-component'; // Adjust the path as necessary
+class ResumeSection extends Component {
+  
+  render() {
+    const {
+      state: {
+        first_name,
+        last_name,
+        phone,
+        email,
+        location,
+        languages,
+        university,
+        university_location,
+        major,
+        gpa,
+        coursework,
+        company1,
+        jobTitle1,
+        startDate1,
+        endDate1,
+        description1,
+        projectTitle1,
+        projectDescription1,
+      },
+      handleChange,
+      handleDateChange,
+      saveUserData,
+    } = this.props;
 
-const DisplayResume = (props) => {
-  // Define state variables using useState
-  const [resumeData, setResumeData] = useState(null);
-  const [error, setError] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
+    return (
+      
+      <StyledResumeBuild> 
+        <br />
 
-  // Function to fetch resume data
-  const getResumeData = async () => {
-    setIsLoading(true);
-    console.log('isLoading State for getResumeData updated to true');
+        <StyledResumeHeader> 
+          <TitleLockup
+            id="resume-build-title"
+            surface="light"
+            textAlignment="center"
+            data={{
+              title: {
+                size: 'title2XLarge',
+                children: (
+                  <>
+                  Resume <span style={redStyleText}>  Builder. </span> 
+                  </>
+                ),
+              },
+            }}
+          />
+        </StyledResumeHeader>
 
-    try {
-      const pathname = window.location.pathname;
-      const user_id_get = pathname.substring(1);
-      console.log("user_id_get is ", user_id_get);
+        <SyledForm> 
+        <form id="resume-build-form"  onSubmit={saveUserData}>
+          <StyledInput> 
+            <Input
+              id="first_name"
+              width="50%"
+              label="First Name"
+              name="first_name"
+              onChange={handleChange}
+              value={first_name}
+              required
+            /> 
+          </StyledInput>
 
-      // Make the GET request using axios
-      const response = await axios.get('api/v1/resume', {
-        params: { user_id: user_id_get },
-      });
+          <StyledInput> 
+          <Input
+            id="last_name"
+            label="Last Name"
+            name="last_name"
+            onChange={handleChange}
+            width="50%"
+            value={last_name}
+            required
+          /> 
+          </StyledInput>
 
-      // Axios automatically parses JSON responses, so you can access data directly
-      setResumeData(response.data);
-      setError(null);
-    } catch (error) {
-      // Handle error
-      console.error('Failed to fetch resume data:', error);
-      setError('Failed to fetch resume data. Please try again later.');
-    } finally {
-      setIsLoading(false);
-      console.log('isLoading State for getResumeData updated to false');
-    }
-  };
+          <StyledInput> 
+          <Input
+            id="phone"
+            label="Phone"
+            name="phone"
+            onChange={handleChange}
+            width="50%"
+            value={phone}
+            required
+          /> 
+          </StyledInput> 
+          
+          <StyledInput> 
+          <Input
+            id="email"
+            label="Email"
+            name="email"
+            onChange={handleChange}
+            width="50%"
+            value={email}
+            required
+          /> 
+          </StyledInput> 
 
-  // Use useEffect to replace componentDidMount
-  useEffect(() => {
-    getResumeData();
-  }, []); // Empty dependency array means this effect runs once when the component mounts
 
-  return (
-    <div>
-      <Loader 
-        active={isLoading}
-        fullscreen={true}
-        surface="light" 
-      />
+          <StyledInput> 
+          <Input
+            id="location"
+            label="Location"
+            name="location"
+            onChange={handleChange}
+            width="50%"
+            value={location}
+            required
+          /> 
+          </StyledInput> 
 
-      {error && <div style={{ color: 'red' }}>{error}</div>}
-      {resumeData && (
-        <div>
-          {/* Render resumeData here */}
-          <pre>{JSON.stringify(resumeData, null, 2)}</pre>
-        </div>
-      )}
-    </div>
-  );
-};
+          <StyledInput> 
+          <Input
+            id="languages"
+            label="Languages"
+            name="languages"
+            onChange={handleChange}
+            width="50%"
+            value={languages}
+            required
+          /> 
+          </StyledInput> 
 
-export default DisplayResume;
+          <StyledInput> 
+          <Input
+            id="university"
+            label="University"
+            name="university"
+            onChange={handleChange}
+            width="50%"
+            value={university}
+            required
+          /> 
+          </StyledInput> 
+
+          <StyledInput> 
+          <Input
+            id="university_location"
+            label="University Location"
+            name="university_location"
+            onChange={handleChange}
+            width="50%"
+            value={university_location}
+            required
+          /> 
+          </StyledInput> 
+
+          
+          <StyledInput> 
+          <Input
+            id="major"
+            label="Major"
+            name="major"
+            onChange={handleChange}
+            width="50%"
+            value={major}
+            required
+          /> 
+          </StyledInput> 
+
+          <StyledInput> 
+          <Input
+            id="gpa"
+            label="GPA"
+            name="gpa"
+            width="50%"
+            onChange={handleChange}
+            value={gpa}
+            required
+          /> 
+          </StyledInput> 
+
+          
+          <StyledInput> 
+          <Input
+            id="coursework"
+            label="Coursework"
+            name="coursework"
+            onChange={handleChange}
+            width="50%"
+            value={coursework}
+            required
+          />  
+          </StyledInput> 
+
+          <StyledInput> 
+          <RadioButtonGroup
+          onChange={() => {}}
+          error={false}
+          data={[
+            {
+              name: 'group',
+              label: 'I have work experience',
+              children: 'You will be prompted to enter a maximum of 3 work experiences',
+              value: 'radioOne',
+              ariaLabel: 'radio one',
+              disabled: false
+            },
+            {
+              name: 'group',
+              label: 'I do not have work experience',
+              children: 'You will not be asked to enter any work experiences',
+              value: 'radioTwo',
+              ariaLabel: 'radio two',
+            }
+          ]}
+          />  
+          </StyledInput>  
+
+          <StyledInput> 
+          <Input
+            id="company1"
+            label="Company"
+            name="company1"
+            onChange={handleChange}
+            width="50%"
+            value={company1}
+            required
+          /> 
+          </StyledInput> 
+
+          <StyledInput> 
+          <Input
+            id="jobTitle1"
+            label="Job Title"
+            name="jobTitle1"
+            onChange={handleChange}
+            width="50%"
+            value={jobTitle1}
+            required
+          /> 
+          </StyledInput> 
+
+          
+          <StyledInput> 
+          <DatePicker
+            id="startDate1"
+            label="Start Date"
+            onChange={(date) => handleDateChange('startDate1', date)}
+            width="70.7%"
+            value={startDate1}
+            required
+          /> 
+          </StyledInput> 
+
+          
+          <StyledInput> 
+          <DatePicker
+            id="endDate1"
+            label="End Date"
+            onChange={(date) => handleDateChange('endDate1', date)}
+            width="70.7%"
+            value={endDate1}
+            required
+          /> 
+          </StyledInput> 
+
+          
+          <StyledInput> 
+          <Input
+            id="description1"
+            label="Description"
+            name="description1"
+            onChange={handleChange}
+            width="50%"
+            value={description1}
+            required
+          />  
+          </StyledInput> 
+
+          
+          <StyledInput> 
+          <RadioButtonGroup
+              onChange={() => {}}
+              error={false}
+              data={[
+                {
+                  name: 'group',
+                  label: 'I have project experience',
+                  children: 'You will be prompted to enter a maximum of 3 project experiences',
+                  value: 'radioOne',
+                  ariaLabel: 'radio one',
+                  disabled: false
+                },
+                {
+                  name: 'group',
+                  label: 'I do not have project experience',
+                  children: 'You will not be asked to enter any project experiences',
+                  value: 'radioTwo',
+                  ariaLabel: 'radio two',
+                }
+              ]}
+            />   
+          </StyledInput> 
+
+          
+          <StyledInput> 
+          <Input
+            id="projectTitle1"
+            label="Project Title"
+            name="projectTitle1"
+            onChange={handleChange}
+            width="50%"
+            value={projectTitle1}
+            required
+          /> 
+          </StyledInput> 
+          
+          <StyledInput> 
+          <Input
+            id="projectDescription1"
+            label="Project Description"
+            name="projectDescription1"
+            onChange={handleChange}
+            width="50%"
+            value={projectDescription1}
+            required
+          /> 
+          </StyledInput> 
+          
+          <StyledResumeButton> 
+            <ButtonGroup
+              id="resume-submit"
+              childWidth="100%"
+              viewport="desktop"
+              surface="light"
+              rowQuantity={{ desktop: 2 }}
+              alignment="center"
+              data={[
+                {
+                  children: 'Save',
+                  size: 'large',
+                  use: 'primary',
+                  width: '100px',
+                  onClick: saveUserData,
+                },
+                {
+                  children: 'Cancel',
+                  size: 'large',
+                  use: 'textLink',
+                  width: '100px',
+                  onClick: () => alert('You clicked the cancel button!'),
+                }
+              ]}
+            />
+          </StyledResumeButton> 
+          <br /> <br />
+        </form>
+        </SyledForm>
+
+      </StyledResumeBuild>
+    );
+  }
+}
+
+export default ResumeSection;
