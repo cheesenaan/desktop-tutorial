7f57749680f35f9efd5c4cce57d889b9e347a940
Sulemaan Farooq <shf46@scarletmail.rutgers.edu>
2024-08-14 13:22:56 -0400

    Update H.js

--- H.js
+++ H.js
@@ -1,58 +1,61 @@
-class ResumeSection extends Component {
-  
-  render() {
-    const {
-      state: {
-        first_name,
-        last_name,
-        phone,
-        email,
-        location,
-        languages,
-        university,
-        university_location,
-        major,
-        gpa,
-        coursework,
-        company1,
-        jobTitle1,
-        startDate1,
-        endDate1,
-        description1,
-        projectTitle1,
-        projectDescription1,
-      },
-      handleChange,
-      handleDateChange,
-      saveUserData,
-    } = this.props;
+import React from 'react';
+import { Input, RadioButtonGroup, DatePicker, ButtonGroup, Loader } from 'your-component-library'; // Adjust imports based on your component library
+import StyledResumeBuild from './StyledResumeBuild'; // Adjust paths as necessary
+import StyledResumeHeader from './StyledResumeHeader';
+import SyledForm from './SyledForm';
+import StyledInput from './StyledInput';
+import StyledResumeButton from './StyledResumeButton';
+import TitleLockup from './TitleLockup'; // Adjust import based on your component library
 
-    return (
-      
-      <StyledResumeBuild> 
-        <br />
+const ResumeSection = ({ 
+  first_name,
+  last_name,
+  phone,
+  email,
+  location,
+  languages,
+  university,
+  university_location,
+  major,
+  gpa,
+  coursework,
+  company1,
+  jobTitle1,
+  startDate1,
+  endDate1,
+  description1,
+  projectTitle1,
+  projectDescription1,
+  handleChange,
+  handleDateChange,
+  saveUserData 
+}) => {
 
-        <StyledResumeHeader> 
-          <TitleLockup
-            id="resume-build-title"
-            surface="light"
-            textAlignment="center"
-            data={{
-              title: {
-                size: 'title2XLarge',
-                children: (
-                  <>
-                  Resume <span style={redStyleText}>  Builder. </span> 
-                  </>
-                ),
-              },
-            }}
-          />
-        </StyledResumeHeader>
+  return (
+    <StyledResumeBuild>
+      <br />
 
-        <SyledForm> 
-        <form id="resume-build-form"  onSubmit={saveUserData}>
-          <StyledInput> 
+      <StyledResumeHeader>
+        <TitleLockup
+          id="resume-build-title"
+          surface="light"
+          textAlignment="center"
+          data={{
+            title: {
+              size: 'title2XLarge',
+              children: (
+                <>
+                  Resume <span style={{ color: 'red' }}> Builder.</span>
+                </>
+              ),
+            },
+          }}
+        />
+      </StyledResumeHeader>
+
+      <SyledForm>
+        <form id="resume-build-form" onSubmit={saveUserData}>
+          <StyledInput>
             <Input
               id="first_name"
               width="50%"
@@ -61,220 +64,213 @@ class ResumeSection extends Component {
               onChange={handleChange}
               value={first_name}
               required
-            /> 
+            />
           </StyledInput>
 
-          <StyledInput> 
-          <Input
-            id="last_name"
-            label="Last Name"
-            name="last_name"
-            onChange={handleChange}
-            width="50%"
-            value={last_name}
-            required
-          /> 
+          <StyledInput>
+            <Input
+              id="last_name"
+              label="Last Name"
+              name="last_name"
+              onChange={handleChange}
+              width="50%"
+              value={last_name}
+              required
+            />
           </StyledInput>
 
-          <StyledInput> 
-          <Input
-            id="phone"
-            label="Phone"
-            name="phone"
-            onChange={handleChange}
-            width="50%"
-            value={phone}
-            required
-          /> 
-          </StyledInput> 
-          
-          <StyledInput> 
-          <Input
-            id="email"
-            label="Email"
-            name="email"
-            onChange={handleChange}
-            width="50%"
-            value={email}
-            required
-          /> 
-          </StyledInput> 
+          <StyledInput>
+            <Input
+              id="phone"
+              label="Phone"
+              name="phone"
+              onChange={handleChange}
+              width="50%"
+              value={phone}
+              required
+            />
+          </StyledInput>
 
+          <StyledInput>
+            <Input
+              id="email"
+              label="Email"
+              name="email"
+              onChange={handleChange}
+              width="50%"
+              value={email}
+              required
+            />
+          </StyledInput>
 
-          <StyledInput> 
-          <Input
-            id="location"
-            label="Location"
-            name="location"
-            onChange={handleChange}
-            width="50%"
-            value={location}
-            required
-          /> 
-          </StyledInput> 
+          <StyledInput>
+            <Input
+              id="location"
+              label="Location"
+              name="location"
+              onChange={handleChange}
+              width="50%"
+              value={location}
+              required
+            />
+          </StyledInput>
 
-          <StyledInput> 
-          <Input
-            id="languages"
-            label="Languages"
-            name="languages"
-            onChange={handleChange}
-            width="50%"
-            value={languages}
-            required
-          /> 
-          </StyledInput> 
+          <StyledInput>
+            <Input
+              id="languages"
+              label="Languages"
+              name="languages"
+              onChange={handleChange}
+              width="50%"
+              value={languages}
+              required
+            />
+          </StyledInput>
 
-          <StyledInput> 
-          <Input
-            id="university"
-            label="University"
-            name="university"
-            onChange={handleChange}
-            width="50%"
-            value={university}
-            required
-          /> 
-          </StyledInput> 
+          <StyledInput>
+            <Input
+              id="university"
+              label="University"
+              name="university"
+              onChange={handleChange}
+              width="50%"
+              value={university}
+              required
+            />
+          </StyledInput>
 
-          <StyledInput> 
-          <Input
-            id="university_location"
-            label="University Location"
-            name="university_location"
-            onChange={handleChange}
-            width="50%"
-            value={university_location}
-            required
-          /> 
-          </StyledInput> 
+          <StyledInput>
+            <Input
+              id="university_location"
+              label="University Location"
+              name="university_location"
+              onChange={handleChange}
+              width="50%"
+              value={university_location}
+              required
+            />
+          </StyledInput>
 
-          
-          <StyledInput> 
-          <Input
-            id="major"
-            label="Major"
-            name="major"
-            onChange={handleChange}
-            width="50%"
-            value={major}
-            required
-          /> 
-          </StyledInput> 
+          <StyledInput>
+            <Input
+              id="major"
+              label="Major"
+              name="major"
+              onChange={handleChange}
+              width="50%"
+              value={major}
+              required
+            />
+          </StyledInput>
 
-          <StyledInput> 
-          <Input
-            id="gpa"
-            label="GPA"
-            name="gpa"
-            width="50%"
-            onChange={handleChange}
-            value={gpa}
-            required
-          /> 
-          </StyledInput> 
+          <StyledInput>
+            <Input
+              id="gpa"
+              label="GPA"
+              name="gpa"
+              width="50%"
+              onChange={handleChange}
+              value={gpa}
+              required
+            />
+          </StyledInput>
 
-          
-          <StyledInput> 
-          <Input
-            id="coursework"
-            label="Coursework"
-            name="coursework"
-            onChange={handleChange}
-            width="50%"
-            value={coursework}
-            required
-          />  
-          </StyledInput> 
+          <StyledInput>
+            <Input
+              id="coursework"
+              label="Coursework"
+              name="coursework"
+              onChange={handleChange}
+              width="50%"
+              value={coursework}
+              required
+            />
+          </StyledInput>
 
-          <StyledInput> 
-          <RadioButtonGroup
-          onChange={() => {}}
-          error={false}
-          data={[
-            {
-              name: 'group',
-              label: 'I have work experience',
-              children: 'You will be prompted to enter a maximum of 3 work experiences',
-              value: 'radioOne',
-              ariaLabel: 'radio one',
-              disabled: false
-            },
-            {
-              name: 'group',
-              label: 'I do not have work experience',
-              children: 'You will not be asked to enter any work experiences',
-              value: 'radioTwo',
-              ariaLabel: 'radio two',
-            }
-          ]}
-          />  
-          </StyledInput>  
+          <StyledInput>
+            <RadioButtonGroup
+              onChange={() => {}}
+              error={false}
+              data={[
+                {
+                  name: 'group',
+                  label: 'I have work experience',
+                  children: 'You will be prompted to enter a maximum of 3 work experiences',
+                  value: 'radioOne',
+                  ariaLabel: 'radio one',
+                  disabled: false
+                },
+                {
+                  name: 'group',
+                  label: 'I do not have work experience',
+                  children: 'You will not be asked to enter any work experiences',
+                  value: 'radioTwo',
+                  ariaLabel: 'radio two',
+                }
+              ]}
+            />
+          </StyledInput>
 
-          <StyledInput> 
-          <Input
-            id="company1"
-            label="Company"
-            name="company1"
-            onChange={handleChange}
-            width="50%"
-            value={company1}
-            required
-          /> 
-          </StyledInput> 
+          <StyledInput>
+            <Input
+              id="company1"
+              label="Company"
+              name="company1"
+              onChange={handleChange}
+              width="50%"
+              value={company1}
+              required
+            />
+          </StyledInput>
 
-          <StyledInput> 
-          <Input
-            id="jobTitle1"
-            label="Job Title"
-            name="jobTitle1"
-            onChange={handleChange}
-            width="50%"
-            value={jobTitle1}
-            required
-          /> 
-          </StyledInput> 
+          <StyledInput>
+            <Input
+              id="jobTitle1"
+              label="Job Title"
+              name="jobTitle1"
+              onChange={handleChange}
+              width="50%"
+              value={jobTitle1}
+              required
+            />
+          </StyledInput>
 
-          
-          <StyledInput> 
-          <DatePicker
-            id="startDate1"
-            label="Start Date"
-            onChange={(date) => handleDateChange('startDate1', date)}
-            width="70.7%"
-            value={startDate1}
-            required
-          /> 
-          </StyledInput> 
+          <StyledInput>
+            <DatePicker
+              id="startDate1"
+              label="Start Date"
+              onChange={(date) => handleDateChange('startDate1', date)}
+              width="70.7%"
+              value={startDate1}
+              required
+            />
+          </StyledInput>
 
-          
-          <StyledInput> 
-          <DatePicker
-            id="endDate1"
-            label="End Date"
-            onChange={(date) => handleDateChange('endDate1', date)}
-            width="70.7%"
-            value={endDate1}
-            required
-          /> 
-          </StyledInput> 
+          <StyledInput>
+            <DatePicker
+              id="endDate1"
+              label="End Date"
+              onChange={(date) => handleDateChange('endDate1', date)}
+              width="70.7%"
+              value={endDate1}
+              required
+            />
+          </StyledInput>
 
-          
-          <StyledInput> 
-          <Input
-            id="description1"
-            label="Description"
-            name="description1"
-            onChange={handleChange}
-            width="50%"
-            value={description1}
-            required
-          />  
-          </StyledInput> 
+          <StyledInput>
+            <Input
+              id="description1"
+              label="Description"
+              name="description1"
+              onChange={handleChange}
+              width="50%"
+              value={description1}
+              required
+            />
+          </StyledInput>
 
-          
-          <StyledInput> 
-          <RadioButtonGroup
+          <StyledInput>
+            <RadioButtonGroup
               onChange={() => {}}
               error={false}
               data={[
@@ -294,35 +290,34 @@ class ResumeSection extends Component {
                   ariaLabel: 'radio two',
                 }
               ]}
-            />   
-          </StyledInput> 
+            />
+          </StyledInput>
 
-          
-          <StyledInput> 
-          <Input
-            id="projectTitle1"
-            label="Project Title"
-            name="projectTitle1"
-            onChange={handleChange}
-            width="50%"
-            value={projectTitle1}
-            required
-          /> 
-          </StyledInput> 
-          
-          <StyledInput> 
-          <Input
-            id="projectDescription1"
-            label="Project Description"
-            name="projectDescription1"
-            onChange={handleChange}
-            width="50%"
-            value={projectDescription1}
-            required
-          /> 
-          </StyledInput> 
-          
-          <StyledResumeButton> 
+          <StyledInput>
+            <Input
+              id="projectTitle1"
+              label="Project Title"
+              name="projectTitle1"
+              onChange={handleChange}
+              width="50%"
+              value={projectTitle1}
+              required
+            />
+          </StyledInput>
+
+          <StyledInput>
+            <Input
+              id="projectDescription1"
+              label="Project Description"
+              name="projectDescription1"
+              onChange={handleChange}
+              width="50%"
+              value={projectDescription1}
+              required
+            />
+          </StyledInput>
+
+          <StyledResumeButton>
             <ButtonGroup
               id="resume-submit"
               childWidth="100%"
@@ -347,14 +342,12 @@ class ResumeSection extends Component {
                 }
               ]}
             />
-          </StyledResumeButton> 
+          </StyledResumeButton>
           <br /> <br />
         </form>
-        </SyledForm>
-
-      </StyledResumeBuild>
-    );
-  }
-}
+      </SyledForm>
+    </StyledResumeBuild>
+  );
+};
 
 export default ResumeSection;
